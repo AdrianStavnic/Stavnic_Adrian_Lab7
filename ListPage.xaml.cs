9d419968558f1311bddd2996dc32772dd091c57e
ADRIAN STAVNIC <adrian.stavnic@stud.ubbcluj.ro>
2023-12-10 20:34:38 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -23,4 +23,19 @@ public partial class ListPage : ContentPage
 		await App.Database.DeleteShopListAsync(slist);
 		await Navigation.PopAsync();
 	}
+
+	async void OnChooseButtonClicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+		{
+			BindingContext = new Product()
+		});
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		var shopl = (ShopList)BindingContext;
+		ListView.ItemsSource = await App.Database.GetListProductAsync(shopl.ID);
+    }
 }
